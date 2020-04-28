ba6b2e2cb29621410db1eadf2c47fdc7221e252a
Salvatore Lauriola <salvatore6324@gmail.com>
2020-04-28 16:48:39 +0200

    Project finished without comments

--- main.js
+++ main.js
@@ -1,3 +1,52 @@
 $(document).ready(function() {
-	//Code
+	
+
+
+//Refs
+var api = "https://flynn.boolean.careers/exercises/api/array/music";
+
+var list = $('.cds-container')
+
+//init Handlebars
+$.ajax({
+		url: api,
+		method: 'GET',
+		success: function(data){
+			// console.log(data);
+			
+			var cds = data.response;
+			var source = $('#disc-template').html();
+			var template = Handlebars.compile(source);
+				
+			for(var i = 0; i < cds.length; i++){
+				var autore = cds[i].author;
+				var copertina = cds[i].poster;
+				var anno = cds[i].year;
+				var titolo = cds[i].title;
+				var context = {
+					poster: copertina,
+					title: titolo,
+					author: autore,
+					year: anno
+				}
+				var html = template(context);
+				list.append(html);
+			}
+		},
+		error: function(){
+			console.log('Errore')
+		}
+})
+
+
+
+
+
+
+
+
+
+
+
+
 });
